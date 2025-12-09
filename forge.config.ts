04cf331c9c68bd5269050f6aa9e8f7c34fa2f697
Vitor Lima <viitorlima2000@gmail.com>
2025-12-09 00:15:19 -0300

    refactor: reorder imports, removes MakerRpm and adds PublisherGithub

--- forge.config.ts
+++ forge.config.ts
@@ -1,16 +1,15 @@
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
+import "dotenv/config";
 import type { ForgeConfig } from "@electron-forge/shared-types";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { mainConfig } from "./webpack.main.config";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
-import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
-import { WebpackPlugin } from "@electron-forge/plugin-webpack";
-import { FusesPlugin } from "@electron-forge/plugin-fuses";
-import { FuseV1Options, FuseVersion } from "@electron/fuses";
 import { PublisherGithub } from "@electron-forge/publisher-github";
-
-import { mainConfig } from "./webpack.main.config";
 import { rendererConfig } from "./webpack.renderer.config";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -18,10 +17,9 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
+    new MakerDeb({}),
     new MakerSquirrel({}),
     new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
@@ -57,7 +55,9 @@ const config: ForgeConfig = {
   ],
   publishers: [
     new PublisherGithub({
-      repository: { name: "electron-calculator", owner: "Vitor Lima" },
+      authToken: process.env.GITHUB_TOKEN,
+      draft: true,
+      repository: { name: "electron-calculator", owner: "vitor-lima-ms" },
     }),
   ],
 };
